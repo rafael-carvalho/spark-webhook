42068ea551fdc32ed704136d538f43d47e859ba7
rafael-carvalho <rafael.lcarvalho@gmail.com>
2016-09-19 12:00:26 -0300

    Update apihelper.py

--- apihelper.py
+++ apihelper.py
@@ -1,7 +1,20 @@
+#   Copyright 2015 maximumG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 '''
 Created on Jul 15, 2016
 
-@author: rafacarv
+@author: Rafael Carvalho (rafacarv@cisco.com)
 '''
 import requests
 import json
@@ -19,7 +32,7 @@ class APICaller(object):
         self.log ("Requesting... " + url)
         response = requests.request(method, url, data=payload, headers=headers)
 
-
+        #HTTP handling found on https://github.com/maximumG/piapi/blob/master/piapi.py
         if response.status_code == 200:
             self.log ("Success")
         elif response.status_code == 302:
